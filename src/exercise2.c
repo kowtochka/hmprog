10b07c868b5688c2883c5e941a1dee7202d34b4c
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-26 19:45:40 +0700

    2try1

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+#include <stdlib.h>
+int main(int argc, char** argv){
+    float array[10];
+    int i;
+    for (i = 0;99>i;i++){
+        scanf("%f",&array[9-i]);
+    }
+    for(i=0;i<9;i++){
+        printf("%.f",array[i]);
+    }
 }
