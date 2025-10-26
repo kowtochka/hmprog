2c74fe9fba9b5bf0c4044b2a03dbe434dba99f59
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-26 21:27:07 +0700

    3try6

--- src/exercise3.c
+++ src/exercise3.c
@@ -8,13 +8,21 @@ int main(int argc,char** argv){
         scanf("%f",&array[i]);
     }
     for(i=0;i<9;i++){
-        i+=K;
+        if(K<0){
+            int j;
+            j = i+K;
+            if(i<0){
+                j=10+i;
+                array[j] = array[i];
+            }
+            array[j] = array[i];
+        }
+        if (K>0)
+        {i+=K;
         if (i>10){
             i = i-10;
         }
-        if(i==-1){
-            i=10;
-        }
-        printf("%.3f\ ",array[i]);
+        printf("%.3f\n",array[i]);
+    }
     }
 }
