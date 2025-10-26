bf0a65d75c4d7433158f0db9134291aaf694c424
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-26 22:45:25 +0700

    4tr9

--- src/exercise4.c
+++ src/exercise4.c
@@ -9,10 +9,10 @@ int main(int argc,char** argv){
     for (i = 0;i<10;i++){
         scanf("%lf",&array[i]);
     }
+    temp1 = array[K];
+    array[9] = 0;
+    array[K] = X;
     for(i=0;i<10;i++){
-        temp1 = array[K];
-        array[9] = 0;
-        array[K] = X;
         while(9-i>K){
             if(9-i == K+1){
                 array[9-i] = temp1;
