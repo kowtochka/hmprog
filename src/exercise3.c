2c47bd7ce847411a40bd75195a8986283395e725
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-26 19:58:47 +0700

    3try1

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+#include <stdlib.h>
+int main(int argc,char** argv){
+    float array[10];
+    int i;
+    int K = atoi(argv[0]);
+    for (i = 0;i<9;i++){
+        scanf("%f",&array[i]);
+    }
+    for(i=0;i<9;i++){
+        i+=K;
+        if (i>10){
+            i = i-10;
+        }
+        printf("%.3f\ ",array[i]);
+    }
 }
