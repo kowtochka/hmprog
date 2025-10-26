7aaf2d112c00203d2bdf30c1f67266a046568faf
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-26 19:31:25 +0700

    1 try

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
+#include <stdlib.h>
+int main(int argc, char** argv){
+    float array[10];
+    int i;
+    float a;
+    for (i = 0;i<10;i++){
+        scanf("%f",&array[i]);
+        a+= array[i]; 
+    }
+    a = a/10;
+    printf("%.3f\n",a);
     return 0;
 }
