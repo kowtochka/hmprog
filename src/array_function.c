1693f123ec3c8ed8266dd6e993a7150d38c0c7fe
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-27 16:33:21 +0700

    1try1

--- src/array_function.c
+++ src/array_function.c
@@ -1,6 +1,15 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    for(int i=0;i<size_src1+size_src2;i++){
+        if (i<size_src1){
+        dest[i]=src1[i];
+            }
+        else{
+        dest[i]=src2[size_src1-i];
+        }
+}
+    }
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
 
