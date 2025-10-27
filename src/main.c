daae4bb13a6604ff80ef5f3ef5a6ed2b4a9962b8
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-27 18:58:39 +0700

    5try6

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 int main(int argc,char** argv){
 int src[10]={1,2,3,4,5,6,7,8,9,10};
 size_t size=10;
-int* min;
-int* max;
+int* min = malloc(128);
+int* max = malloc(128);
 get_min_and_max_from_int_array(src,size,min,max);
 printf("%d, %d\n",*min,*max);
 return 0;
