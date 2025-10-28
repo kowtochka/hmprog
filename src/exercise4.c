3aa3b35b05419d3006c4a70b16970a35c9d8ce62
Саая Аяс ИП-515 <stud@localhost.localdomain>
2025-10-28 14:39:33 +0700

    4try9

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc,char** argv){
-    double array[10];
-    int i;
-    double temp1;
-    double X = atof(argv[0]);
-    int K = atoi(argv[1]);
-    for (i = 0;i<10;i++){
+int main(int argc, char** argv) {
+double array[10];
+double X = atof(argv[1]);
+int K = atoi(argv[0]);
+  for (i = 0;i<10;i++){
         scanf("%lf",&array[i]);
     }
-    temp1 = array[K];
-    array[9] = 0;
-    array[K] = X;
-    for(i=0;i<10;i++){
-        while(9-i>K){
-            if(9-i == K+1){
-                array[9-i] = temp1;
-            }
-            if(9-i!=K+1){
-                array[9-i] = array[9-1-i];
-           }
-        }
+for(int i=9;i>K;i--){
+    array[i] = array[i-1];
+}
+array[K] = X;
+ for (i=0;i<10;i++){
+        printf("%.lf\n",array[i]);
     }
-    for (i=0;i<10;i++){
-        printf("%.3lf\n",array[i]);
-    }
+    return 0;
 }
