bf00551a9c2a036e1189868f35b1c39326e39f25
Bensler_Konstantin <beslerkostai@gmail.com>
2025-10-30 20:16:30 +0700

    2tryhz

--- src/array_function.c
+++ src/array_function.c
@@ -11,17 +11,26 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
     }
 }
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-        for(size_t i=0;i<size_src1+size_src2;i+=2){
-        int temp1=src1[i];
-        int temp2=src2[i];
-    if (temp1>temp2){
-        dest[i]=temp2;
+        for(size_t i=0;i<size_src1;i++){
+        dest[i]=src1[i];
+            }
+    for(size_t i=0;i<size_src2;i++){
+        dest[i+size_src1]=src2[i];
     }
-    if (temp1<temp2){
-        dest[i]=temp1;
-    }    
-}
-}
+      for (size_t step = 0; step < (size_src1+size_src2) - 1; ++step) {
+    int swap = 0;
+    for (int i = 0; i < (size_src1+size_src2) - step - 1; ++i) {
+      if (dest[i] > dest[i + 1]) {
+        int temp = dest[i];
+        dest[i] = dest[i + 1];
+        dest[i + 1] = temp;
+        swap = 1;
+      }
+    }
+    if (swap == 0) {
+      break;
+    }
+  }
 
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
     int tm[2];
